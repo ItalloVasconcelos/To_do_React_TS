50c1fb888fe57a00a5bc13de203cd32403399bd1
Itallo Vasconcelos <itallo.savio.vasconcelos61@ifce.aluno.edu.br>
2022-07-07 15:43:49 -0300

    feat: finhised the code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,35 +1,85 @@
-import React, {useState} from 'react'
-import styles from "./Home.module.css"
-import Footer from '../../components/Footer/Footer'
-import Header from '../../components/Header/Header'
-import TaskForm from '../../components/TaskForm/TaskForm'
-import TaskList from '../../components/TaskList/TaskList'
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import TaskForm from "../../components/TaskForm/TaskForm";
+import TaskList from "../../components/TaskList/TaskList";
 
-import { ITask } from '../../interface/Task'
-
-
+import { ITask } from "../../interface/Task";
+import Modal from "../../components/Modal/Modal";
 
 const Home = () => {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
-  const [taskList, setTaskList] = useState<ITask[]>([])
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const hideOrShow = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShow(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = {id, title, difficulty}
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task
+    })
+    setTaskList(updatedItems)
+    hideOrShow(false)
+  }
   return (
     <div>
-        <Header />
-    <main className={styles.main}>
-    <div>
-      <h2>O que você vai fazer?//</h2>
-      <TaskForm btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList} />
-    </div>
-    <div>
-      <h2>Suas tarefas</h2>
-      <TaskList taskList={taskList}/>
-    </div>
-    </main>
-    
+      <Modal
+        children={
+          <TaskForm
+            btnText="Editar Tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?//</h2>
+          <TaskForm
+            btnText="Criar tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+            
+          />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
 
-        <Footer />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
